048ff66c5b30d6661b61b2ad96bce0f34eb447a3
Michael21Official <michael.2001.official@gmail.com>
2026-04-22 12:05:10 +0200

    Initial project

---
+++ src/test/java/com/remitly/stockmarket/StockMarketSimulatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.remitly.stockmarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockMarketSimulatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
