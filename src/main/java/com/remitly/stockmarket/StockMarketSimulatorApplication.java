9e26f6c1d91fd2582e8ebe917af2d7236786884a
Michael21Official <michael.2001.official@gmail.com>
2026-04-22 12:30:10 +0200

    Configure database, add Retry, Swagger and enable caching

--- src/main/java/com/remitly/stockmarket/StockMarketSimulatorApplication.java
+++ src/main/java/com/remitly/stockmarket/StockMarketSimulatorApplication.java
@@ -2,8 +2,12 @@ package com.remitly.stockmarket;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.retry.annotation.EnableRetry;
 
 @SpringBootApplication
+@EnableCaching
+@EnableRetry
 public class StockMarketSimulatorApplication {
 
 	public static void main(String[] args) {
